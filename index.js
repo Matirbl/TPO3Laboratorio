468f6afd9964d3fa8c2b48d31cbe20e567b290c4
Mati <mati.rbl@gmail.com>
2021-11-29 19:59:08 -0300

    creando servidor

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const path = require("path");
+const morgan = require("morgan");
+const data = require("./instrumentos.json");
 const app = express();
 
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/indice.html"));
+  res.sendFile(path.join(__dirname + "/public/indice.html"));
 });
 
 app.get("/instrumento", (req, res) => {
@@ -13,18 +16,21 @@ app.get("/instrumento", (req, res) => {
   res.send("get recibido");
 });
 
-app.post("/instrumento", (req, res) => {
-  res.send("post recibido");
-});
-
-
 app.post("/instrumento/:id", (req, res) => {
   console.log(req.body);
-  console.log(req.params.id);
+  console.log(req.params);
   res.send("se agregó un instrumento");
 });
 
+app.put("/instrumento/:id", (req, res) => {
+  console.log(req.body);
+  res.send("user " + req.params.id + " actualizado");
+});
+
 console.log(__dirname);
+
+app.use(express.static("public"));
+
 app.listen(5000, () => {
   console.log("server running on port", 5000);
 });
